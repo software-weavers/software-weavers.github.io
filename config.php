aff3a1b3a761a21df8c8fed42d9a8b2415230328
N. Fasano <fasano.nm@gmail.com>
2025-04-24 17:54:31 +0300

    Mobile version

--- config.php
+++ config.php
@@ -2,4 +2,15 @@
 
 require_once './i18n/i18n.php';
 
-define('LANG', Language::from(getenv('APP_LANG') ?: 'en'));
+define('LANG', Language::from(getenv('APP_LANG') ?: 'fr'));
+
+function nav_link(string $path, string $content, ?string $currentPath = null): string {
+    $selected = $path === $currentPath
+        ? 'class="selected"'
+        : '';
+
+    $path = i18n($path);
+    $content = i18n($content);
+
+    return "<a {$selected} href=\"{$path}\">{$content}</a>";
+}
